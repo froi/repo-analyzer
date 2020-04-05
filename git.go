8e2a12a9a931470e53d71602604b0c39913787ae
Froilan Irizarry <froilan@froilanirizarry.me>
2020-04-05 19:49:43 -0400

    Initial commit of git module Co-authored-by: dannywolfmx
    Signed-off-by: Froilan Irizarry <froilan@froilanirizarry.me>

---
+++ git.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
+type Git struct {
+	branch string
+	cmd    string
+	err    error
+}
+
+func executeCommand(commandString string, subcommand string, cmdArgs []string) error {
+
+	cmdArgs = append([]string{subcommand}, cmdArgs...)
+	cmd := exec.Command(commandString, cmdArgs...)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	return cmd.Run()
+}
+
+func (git *Git) Push(commandArgs ...string) *Git {
+	if git.err != nil {
+		return git
+	}
+	git.err = executeCommand(git.cmd, "push", commandArgs)
+	return git
+}
+
+func (git *Git) Add(commandArgs ...string) *Git {
+	if git.err != nil {
+		return git
+	}
+	git.err = executeCommand(git.cmd, "add", commandArgs)
+
+	return git
+}
+
+func (git *Git) Commit(commandArgs ...string) *Git {
+	if git.err != nil {
+		return git
+	}
+
+	git.err = executeCommand(git.cmd, "commit", commandArgs)
+
+	return git
+}
+
+func (git *Git) AddToLfs(fileExtension string) *Git {
+	if git.err != nil {
+		return git
+	}
+	cmdArgs := []string{"track", fmt.Sprintf("*.%s", fileExtension)}
+	git.err = executeCommand(git.cmd, "lfs", cmdArgs)
+
+	return git
+}
+
+func (git *Git) Error() error {
+	return git.err
+}
